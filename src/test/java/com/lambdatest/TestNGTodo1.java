cea30596b928ea580be8952e15e6cf7e1936fb83
unknown <lambdatestgithub@gmail.com>
2023-10-06 00:48:48 +0530

    first commit

--- src/test/java/com/lambdatest/TestNGTodo1.java
+++ src/test/java/com/lambdatest/TestNGTodo1.java
@@ -9,9 +9,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
 public class TestNGTodo1 {
 
@@ -19,19 +18,19 @@ public class TestNGTodo1 {
     private String Status = "failed";
 
     @BeforeMethod
-    public void setup(Method m, ITestContext ctx) throws MalformedURLException {
+    public void setup() throws MalformedURLException {
         String username = System.getenv("LT_USERNAME") == null ? "Your LT Username" : System.getenv("LT_USERNAME");
         String authkey = System.getenv("LT_ACCESS_KEY") == null ? "Your LT AccessKey" : System.getenv("LT_ACCESS_KEY");
-        ;
         String hub = "@hub.lambdatest.com/wd/hub";
 
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("platform", "MacOS Catalina");
-        caps.setCapability("browserName", "Safari");
+        caps.setCapability("platform", "Windows 10");
+        caps.setCapability("browserName", "Chrome");
         caps.setCapability("version", "latest");
-        caps.setCapability("build", "TestNG With Java");
-        caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
+        caps.setCapability("build", "TestNG JAVA Method");
+//        caps.setCapability("name", m.getName() + " - " + this.getClass().getName());
         caps.setCapability("plugin", "git-testng");
+        caps.setCapability("selenium_version", "4.12.0");
 
         String[] Tags = new String[] { "Feature", "Falcon", "Severe" };
         caps.setCapability("tags", Tags);
@@ -47,52 +46,78 @@ public class TestNGTodo1 {
 
         driver.get("https://lambdatest.github.io/sample-todo-app/");
 
-        System.out.println("Checking Box");
-        driver.findElement(By.name("li1")).click();
+//        System.out.println("Checking Box");
+//        driver.findElement(By.name("li1")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li2")).click();
+//
+//        System.out.println("Checking Box");
+//        driver.findElement(By.name("li3")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li4")).click();
+//
+//        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
+//        driver.findElement(By.id("addbutton")).click();
+//
+//        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
+//        driver.findElement(By.id("addbutton")).click();
+//
+//        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
+//        driver.findElement(By.id("addbutton")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li1")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li3")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li7")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li8")).click();
+//        Thread.sleep(300);
+//
+//        System.out.println("Entering Text");
+//        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
+//
+//        driver.findElement(By.id("addbutton")).click();
+//
+//        System.out.println("Checking Another Box");
+//        driver.findElement(By.name("li9")).click();
+//
+//        // Let's also assert that the todo we added is present in the list.
+//
+//        spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
+//        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
+        Status = "passed";
+        Thread.sleep(150);
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li2")).click();
+        System.out.println("TestFinished");
 
-        System.out.println("Checking Box");
-        driver.findElement(By.name("li3")).click();
+    }
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li4")).click();
+    @Test
+    public void basicTest1() throws InterruptedException {
+        System.out.println("Loading Url2");
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 6");
-        driver.findElement(By.id("addbutton")).click();
+        driver.get("https://google.co.in");
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 7");
-        driver.findElement(By.id("addbutton")).click();
+        Status = "passed";
+        Thread.sleep(150);
 
-        driver.findElement(By.id("sampletodotext")).sendKeys(" List Item 8");
-        driver.findElement(By.id("addbutton")).click();
+        System.out.println("TestFinished");
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li1")).click();
+    }
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li3")).click();
+    @Test
+    public void basicTest2() throws InterruptedException {
+        System.out.println("Loading Url3");
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li7")).click();
+        driver.get("https://google.co.in");
 
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li8")).click();
-        Thread.sleep(300);
-
-        System.out.println("Entering Text");
-        driver.findElement(By.id("sampletodotext")).sendKeys("Get Taste of Lambda and Stick to It");
-
-        driver.findElement(By.id("addbutton")).click();
-
-        System.out.println("Checking Another Box");
-        driver.findElement(By.name("li9")).click();
-
-        // Let's also assert that the todo we added is present in the list.
-
-        spanText = driver.findElementByXPath("/html/body/div/div/div/ul/li[9]/span").getText();
-        Assert.assertEquals("Get Taste of Lambda and Stick to It", spanText);
         Status = "passed";
         Thread.sleep(150);
 
@@ -101,7 +126,7 @@ public class TestNGTodo1 {
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
         driver.executeScript("lambda-status=" + Status);
         driver.quit();
     }
